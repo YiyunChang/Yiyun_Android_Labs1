c86a6e74f375cfdcef2a9f08d7bd6283e45e7ccf
Yiyun Chang <chan0544@algonquinlive.com>
2023-09-15 16:26:42 -0400

    Try again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 android {
     namespace = "algonquin.cst2335.yiyun"
     compileSdk = 33
-
+    buildFeatures{
+        viewBinding = true
+    }
     defaultConfig {
         applicationId = "algonquin.cst2335.yiyun"
         minSdk = 22
